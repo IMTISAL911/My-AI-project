800c33c1b28f0e922e5c99463d72dd746775aa9e
IMTISAL HUSSAIN <imtisalhussan@gmail.com>
2026-02-05 20:39:51 +0500

    removing commits and spaces

--- app/chatt/layout.jsx
+++ app/chatt/layout.jsx
@@ -1,26 +1,3 @@
-// "use client";
-// import { useRouter } from "next/navigation";
-// import { useEffect } from "react";
-// // import Sidebar from "@/components/Sidebar"; // correct import
-// import Sidebar from "../components/Sidebar";
-
-// export default function ChatLayout({ children }) {
-//   const router = useRouter();
-
-//   useEffect(() => {
-//     const user = localStorage.getItem("user");
-//     if (!user) {
-//       router.push("/auth/login"); // redirect if not logged in
-//     }
-//   }, [router]);
-
-//   return (
-//     <div className="flex h-screen bg-gray-900 text-white">
-//       <Sidebar />
-//       <main className="flex-1 overflow-hidden">{children}</main>
-//     </div>
-//   );
-// }
 
 
 
--- app/chatt/page.jsx
+++ app/chatt/page.jsx
@@ -1,8 +1,3 @@
-// import ChatArea from "@/app/components/ChatArea"; 
-
-// export default function ChatPage() {
-//   return <ChatArea />;
-// }
 
 
 
